45fcd7c2a960f097e5cc8facc78ea98917cacd4c
scrov <you@example.com>
2026-09-10 17:32:42 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,15 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-//*        vite: {
+/*        vite: {
                 plugins: [tailwindcss()],
                 resolve: {
                         alias: {
                                 "@": fileURLToPath(new URL("./src", import.meta.url)),
 			},
 		},
-	},*/
+	},
+*/
 	site: "https://scrov.app",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
