53e6c4cc27fc99ec710d50f157261a3c07f3f062
scrov <you@example.com>
2026-09-10 18:10:53 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { fileURLToPath, URL } from "node:url";
+//import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-/*        vite: {
-                plugins: [tailwindcss()],
-                resolve: {
-                        alias: {
-                                "@": fileURLToPath(new URL("./src", import.meta.url)),
-			},
-		},
-	},
-*/
 	site: "https://scrov.app",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
