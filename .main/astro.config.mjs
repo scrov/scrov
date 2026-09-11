3f0bbe605ad01a504a876398b065828f53467bc0
scrov <you@example.com>
2026-09-10 20:59:05 -0400

    .

--- .main/astro.config.mjs
+++ .main/astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
-
-import { fileURLToPath, URL } from "node:url";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+//import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				"@": fileURLToPath(new URL("./src", import.meta.url)),
-			},
+	site: "https://scrov.app",
+	integrations: [mdx(), sitemap()],
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
 		},
-	},
+	}),
 });
+
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,34 @@
 // @ts-check
+
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-//import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
 	site: "https://scrov.app",
-	integrations: [mdx(), sitemap()],
+
+	integrations: [
+		mdx(),
+		sitemap(),
+	],
+
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		},
+	},
+
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
 		},
 	}),
 });
-
